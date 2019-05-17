d369bd28e790f33d5779a0d713a04817e1316a47
marieauxille denis <marieauxille@gmail.com>
2019-05-17 12:00:36 +0200

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+Task.create title: 'Laundry', details: 'Do not mix colors!'
+Task.create title: 'Studying', details: 'A lot of flashcards to do', completed: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
